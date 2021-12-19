d3431df6d74678af8e59bdd53132c2108f1c5907
alva-im <52602843+alva-im@users.noreply.github.com>
2021-12-19 18:35:02 +0800

    获取每一个目录的天数差距

--- data_maintenance.py
+++ data_maintenance.py
@@ -1,13 +1,17 @@
 # coding: utf-8
-
+import datetime
 import os
 from config import DATA_BASE
 
 
 def list_dirs():
     for dir in os.listdir(DATA_BASE):
-        print(dir)
-
+        try:
+            dt = datetime.datetime.strptime(dir, '%Y-%m-%d')
+            days = (datetime.datetime.now() - dt).days
+            print(f'{dir} -> {days}')
+        except:
+            pass
 
 def main():
     list_dirs()
